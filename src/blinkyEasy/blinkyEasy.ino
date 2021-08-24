ed73e2d7af137fc261c25d04b5d81d3fc1dee087
MiguelAmaro <miguelamaro100@gmail.com>
2021-08-24 09:26:39 -0400

    created blinky project

--- src/blinkyEasy/blinkyEasy.ino
+++ src/blinkyEasy/blinkyEasy.ino
@@ -9,12 +9,12 @@ int ledPin = 12;
 /// Our Routines
 void blinkLed(int pin)
 {
-  digitalWrite(pin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second aka do useless work to occupy the processor
-  digitalWrite(pin, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second aka do useless work to occupy the processor
+	digitalWrite(pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+	delay(1000);               // wait for a second aka do useless work to occupy the processor
+	digitalWrite(pin, LOW);    // turn the LED off by making the voltage LOW
+	delay(1000);               // wait for a second aka do useless work to occupy the processor
 
-  return;
+	return;
 }
 
 /// Arduino IDE Default Routines 
@@ -22,7 +22,7 @@ void setup(void)
 {
 	//NOTE(): Configures GPIO(General Purpose Input Output) pin
 	pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(ledPin, OUTPUT);
+	pinMode(ledPin, OUTPUT);
   
 	//NOTE(): make sure computer your connected to
 	//        uses the same baud rate(9600).
@@ -33,9 +33,9 @@ void loop(void)
 {
 	// put your main code here, to run repeatedly:
 	Serial.println("blinking...");
-  #if USE_ONBOARD_LED
-  blinkLed(LED_BUILTIN);
-  #else
-  blinkLed(ledPin);
-  #endif
+	#if USE_ONBOARD_LED
+	blinkLed(LED_BUILTIN);
+	#else
+	blinkLed(ledPin);
+	#endif
 }
